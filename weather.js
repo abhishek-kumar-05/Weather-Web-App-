946e08f53f57f837777830d75a7ef7c279c178bc
abhishek-kumar-05 <kumarabhi0513@gmail.com>
2024-07-10 00:31:19 +0530

    updated weather.js (getting value from input field)

--- weather.js
+++ weather.js
@@ -1,6 +1,25 @@
-document
-  .querySelector(".searchBar button")
-  .addEventListener("click", function () {
-    const place = document.getElementById("searchValue").value;
-    fetchWeather(place);
-  });
+// validateInputField function check wheter the input is empty or not 
+// if the field is not empty then it do futher process otherwise it will raise a alert
+const validateInputField = () => {
+  const location = document.getElementById("searchValue").value;
+  if (location !== "") {
+    document.getElementById("searchValue").value = "";
+    console.log(location);
+  } else {
+    alert("Enter the location value");
+  }
+};
+
+
+
+
+// getting button element and adding eventlistner
+const button = document.querySelector(".search-button");
+button.addEventListener("click", function () {
+  validateInputField();
+});
+// getting input field element and adding eventlistner
+const inputField = document.getElementById("searchValue");
+inputField.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") validateInputField();
+});
